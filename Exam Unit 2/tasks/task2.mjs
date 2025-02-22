152b3f4b283339a0083a056b71285384a910ce1f
Oskar <oskardb@uia.no>
2025-02-22 16:07:24 +0100

    flatten nested arrays

---
+++ Exam Unit 2/tasks/task2.mjs
@@ -0,0 +1,12 @@
+
+function flatten(arr) {
+    const flattened = [];
+
+    for (const item of arr) {
+        if (Array.isArray(item))
+            flattened.push(...flatten(item));
+        else
+            flattened.push(item);
+    }
+    return flattened;
+}
