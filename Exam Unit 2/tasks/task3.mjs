f9025b310e4eeae316c130df2ce1b7aa4d3d68b1
Oskar <oskardb@uia.no>
2025-02-22 19:43:55 +0100

    fixed file pathing, and moved main function

--- Exam Unit 2/tasks/task3.mjs
+++ Exam Unit 2/tasks/task3.mjs
@@ -1,19 +1,52 @@
 import fs from "fs";
+import path from "path";
 
-async function loadData() {
+function loadTreeFromFile(filepath) {
     try {
-        const data = fs.readFileSync('nodes.json', 'utf8');
+        const data = fs.readFileSync(filepath, 'utf8');
         return JSON.parse(data);
-    } catch (err) {
+    }catch (err) {
         console.error('Error loading JSON file:', err);
         return null;
     }
 }
 
-async function main() {
-    const root = await loadData();
+    function analyzeTree(node, depth = 0, results = { sum: 0, maxDepth: 0, nodeCount: 0}) {
+        if (node === null) {
+            return results;
+        }
 
-    if (!root) {
-        process.exit(1);
+        results.sum = results.sum + node.value;
+        results.nodeCount = results.nodeCount + 1;
+
+        if (depth > results.maxDepth) {
+            results.maxDepth = depth;
+        }
+
+        if (node.left !== null) {
+            analyzeTree(node.left, depth + 1, results);
+        }
+
+        if (node.right !== null) {
+            analyzeTree(node.right, depth + 1, results);
+        }
+        return results;
     }
+
+    function main() {
+        const __dirname = import.meta.dirname;
+        const filepath = path.join(__dirname, "..", "example_files", "nodes.json")
+        const root = loadTreeFromFile(filepath);
+    
+        if (!root) {
+            process.exit(1);
+        }
+
+    const treeResults = analyzeTree(root);
+
+    console.log('Sum of all values: ', treeResults.sum);
+    console.log('Deepest level: ', treeResults.maxDepth + 1);
+    console.log('Number of nodes: ', treeResults.nodeCount);
 }
+
+main();
