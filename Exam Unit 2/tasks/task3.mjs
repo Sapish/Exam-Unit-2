c7db8492a64eefef69271a276768f72f21e9f469
Oskar <oskardb@uia.no>
2025-02-22 18:45:19 +0100

    function to load the data from nodes.json

---
+++ Exam Unit 2/tasks/task3.mjs
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+async function loadData() {
+    try {
+        const data = fs.readFileSync('nodes.json', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error loading JSON file:', err);
+        return null;
+    }
+}
